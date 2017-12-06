8ef76a3b8c3ce7b08ddfe04ec8ac2b6153002a44
Gálffy Tamás <ezittgtx@gmail.com>
2017-12-06 19:31:58 +0100

    Extended fullwidth's character map It also uses stdin now for pipeability

--- fullwidth.sh
+++ fullwidth.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 
-# Convert arguments into ｆｕｌｌｗｉｄｔｈ text for some of those sweet-sweet ｒｏｂｕｓｔ memes.
+# Convert input into ｆｕｌｌｗｉｄｔｈ text for some of those sweet-sweet ｒｏｂｕｓｔ memes.
 # Currently, it only supports a simple set of english characters
 
 # Usage:
-# 	fullwidth text...
+# 	cat ./navy-seal-copypasta.txt | fullwidth
+#	echo xXx_gAngstA_420_xXx | fullwidth
 
 function fullwidth {
-	str="$@"
-
-	abc="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	abc=""
+	abc="${abc}!\"#$%&'()*+,-./ "
+	abc="${abc}0123456789"
+	abc="${abc}:;<=>?@"
+	abc="${abc}ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	abc="${abc}[\\]^_\`"
 	abc="${abc}abcdefghijklmnopqrstuvwxyz"
+	abc="${abc}{|}~"
 
-	fbc="ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"
+	fbc=""
+	fbc="${fbc}！＂＃＄％＆＇（）＊＋，－．／　"
+	fbc="${fbc}０１２３４５６７８９"
+	fbc="${fbc}：；＜＝＞？＠"
+	fbc="${fbc}ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"
+	fbc="${fbc}［＼］＾＿｀"
 	fbc="${fbc}ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"
+	fbc="${fbc}｛｜｝～"
 
-	for (( i=0; i<${#str}; i++ )); do
-		str_c="${str:$i:1}"
-		fbc_c="$str_c"
-		for (( j=0; j<${#abc}; ++j )); do
-			abc_c="${abc:$j:1}"
-			if [ "$str_c" = "$abc_c" ] 
-			then
-				fbc_c="${fbc:$j:1}"
-			fi
+	while read str; do
+		for (( i=0; i<${#str}; i++ )); do
+			str_c="${str:$i:1}"
+			fbc_c="$str_c"
+			for (( j=0; j<${#abc}; ++j )); do
+				abc_c="${abc:$j:1}"
+				if [ "$str_c" = "$abc_c" ] 
+				then
+					fbc_c="${fbc:$j:1}"
+				fi
+			done
+
+			printf "$fbc_c"
 		done
 
-		printf "$fbc_c"
+		printf "\n"
 	done
-
-	printf "\n"
 }
